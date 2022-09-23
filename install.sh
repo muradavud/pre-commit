366c1cb9770678ef725f4017b6a86b19c95a9f02
m.davidov <murad.davidov@technofusion.az>
2022-09-23 17:07:11 +0400

    install.sh configured

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+echo "$PWD"/.git/hooks/pre-commit
+
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
+
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.49.0 &&
 
-cp ./pre-commit.sh ../.git/hooks/pre-commit &&
-cp ./commit-msg.sh ../.git/hooks/commit-msg &&
+cp "$BASEDIR"/pre-commit.sh "$PWD"/.git/hooks/pre-commit &&
+cp "$BASEDIR"/commit-msg.sh "$PWD"/.git/hooks/commit-msg &&
 
-chmod 755 ../.git/hooks/pre-commit &&
-chmod 755 ../.git/hooks/commit-msg &&
+chmod 755 "$PWD"/.git/hooks/pre-commit &&
+chmod 755 "$PWD"/.git/hooks/commit-msg &&
 
 echo "
 linters:
@@ -19,4 +24,4 @@ linters:
   - staticcheck
   - typecheck
   - unused
-" > ../.golangci.yaml
+" > "$PWD"/.golangci.yaml
